fcadbf86fa62382b721e3130893be8b7c5e6acd2
lansanac <lansana.camara@outlook.it>
2025-08-10 15:05:57 +0200

    Aggiungere i file di progetto.

---
+++ PrestitiBiblioteca/Controllers/PrestitiController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PrestitiBiblioteca.Models;
+
+namespace PrestitiBiblioteca.Controllers
+{
+    public class PrestitiController : Controller
+    {
+        private readonly PrestitiBibliotecaContext _context;
+
+        public PrestitiController(PrestitiBibliotecaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Prestiti
+        public async Task<IActionResult> Index()
+        {
+            var prestitiBibliotecaContext = _context.Prestitos.Include(p => p.IdLibroNavigation).Include(p => p.MatricolaNavigation);
+            return View(await prestitiBibliotecaContext.ToListAsync());
+        }
+
+        // GET: Prestiti/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var prestito = await _context.Prestitos
+                .Include(p => p.IdLibroNavigation)
+                .Include(p => p.MatricolaNavigation)
+                .FirstOrDefaultAsync(m => m.IdLibro == id);
+            if (prestito == null)
+            {
+                return NotFound();
+            }
+
+            return View(prestito);
+        }
+
+        // GET: Prestiti/Create
+        public IActionResult Create()
+        {
+            ViewData["IdLibro"] = new SelectList(_context.Libros, "Codice", "Codice");
+            ViewData["Matricola"] = new SelectList(_context.Studentes, "Matricola", "Matricola");
+            return View();
+        }
+
+        // POST: Prestiti/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdLibro,Matricola,DataPrestito,DataRestituzione")] Prestito prestito)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(prestito);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdLibro"] = new SelectList(_context.Libros, "Codice", "Codice", prestito.IdLibro);
+            ViewData["Matricola"] = new SelectList(_context.Studentes, "Matricola", "Matricola", prestito.Matricola);
+            return View(prestito);
+        }
+
+        // GET: Prestiti/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var prestito = await _context.Prestitos.FindAsync(id);
+            if (prestito == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdLibro"] = new SelectList(_context.Libros, "Codice", "Codice", prestito.IdLibro);
+            ViewData["Matricola"] = new SelectList(_context.Studentes, "Matricola", "Matricola", prestito.Matricola);
+            return View(prestito);
+        }
+
+        // POST: Prestiti/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdLibro,Matricola,DataPrestito,DataRestituzione")] Prestito prestito)
+        {
+            if (id != prestito.IdLibro)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(prestito);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PrestitoExists(prestito.IdLibro))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdLibro"] = new SelectList(_context.Libros, "Codice", "Codice", prestito.IdLibro);
+            ViewData["Matricola"] = new SelectList(_context.Studentes, "Matricola", "Matricola", prestito.Matricola);
+            return View(prestito);
+        }
+
+        // GET: Prestiti/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var prestito = await _context.Prestitos
+                .Include(p => p.IdLibroNavigation)
+                .Include(p => p.MatricolaNavigation)
+                .FirstOrDefaultAsync(m => m.IdLibro == id);
+            if (prestito == null)
+            {
+                return NotFound();
+            }
+
+            return View(prestito);
+        }
+
+        // POST: Prestiti/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var prestito = await _context.Prestitos.FindAsync(id);
+            if (prestito != null)
+            {
+                _context.Prestitos.Remove(prestito);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PrestitoExists(int id)
+        {
+            return _context.Prestitos.Any(e => e.IdLibro == id);
+        }
+    }
+}
---
+++ PrestitiBiblioteca/Controllers/StudentiController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PrestitiBiblioteca.Models;
+
+namespace PrestitiBiblioteca.Controllers
+{
+    public class StudentiController : Controller
+    {
+        private readonly PrestitiBibliotecaContext _context;
+
+        public StudentiController(PrestitiBibliotecaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Studenti
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Studentes.ToListAsync());
+        }
+
+        // GET: Studenti/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var studente = await _context.Studentes
+                .FirstOrDefaultAsync(m => m.Matricola == id);
+            if (studente == null)
+            {
+                return NotFound();
+            }
+
+            return View(studente);
+        }
+
+        // GET: Studenti/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Studenti/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Matricola,Nome,Cognome,Email,Classe")] Studente studente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(studente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(studente);
+        }
+
+        // GET: Studenti/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var studente = await _context.Studentes.FindAsync(id);
+            if (studente == null)
+            {
+                return NotFound();
+            }
+            return View(studente);
+        }
+
+        // POST: Studenti/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Matricola,Nome,Cognome,Email,Classe")] Studente studente)
+        {
+            if (id != studente.Matricola)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(studente);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudenteExists(studente.Matricola))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(studente);
+        }
+
+        // GET: Studenti/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var studente = await _context.Studentes
+                .FirstOrDefaultAsync(m => m.Matricola == id);
+            if (studente == null)
+            {
+                return NotFound();
+            }
+
+            return View(studente);
+        }
+
+        // POST: Studenti/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var studente = await _context.Studentes.FindAsync(id);
+            if (studente != null)
+            {
+                _context.Studentes.Remove(studente);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StudenteExists(int id)
+        {
+            return _context.Studentes.Any(e => e.Matricola == id);
+        }
+    }
+}
